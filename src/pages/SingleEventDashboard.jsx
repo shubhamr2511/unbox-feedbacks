8e5fcbc463b0b5f5e234e1e73fb2abf4bb049589
shubhamr2511 <shubhamr2511@gmail.com>
2024-09-26 10:34:10 +0530

    remove caution funtion

--- src/pages/SingleEventDashboard.jsx
+++ src/pages/SingleEventDashboard.jsx
@@ -52,61 +52,61 @@ function SingleEventDashboard() {
   }, []);
 
 
-  const caution = () => {
-    const pp = [
-      { "name": "Abhishek", "code": "9042", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Aditya", "code": "7836", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Amit", "code": "3388", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Aravind", "code": "1971", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Arnav", "code": "4880", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Ashwini", "code": "9322", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Atul", "code": "6643", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Chandrasekar", "code": "2723", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Chirag", "code": "6740", "fbReceived": 0, "fbSent": 0 },
-      { "name": "DhruvD", "code": "9668", "fbReceived": 0, "fbSent": 0 },
-      { "name": "DhruvK", "code": "5035", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Donnie", "code": "8702", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Emanda", "code": "9475", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Gowri", "code": "3624", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Hriday", "code": "2188", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Jitu", "code": "3992", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Kahraman", "code": "8446", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Kaimal", "code": "5444", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Karan", "code": "6256", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Kohli", "code": "8014", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Mandanna", "code": "9183", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Manjiri", "code": "1611", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Manoj", "code": "5596", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Maria", "code": "9210", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Naina", "code": "2386", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Naveen", "code": "6942", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Neel", "code": "2076", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Paul", "code": "3697", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Pradeep", "code": "9236", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Prasad", "code": "8425", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Priti", "code": "7219", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Raghavendra", "code": "6781", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Raghu", "code": "3924", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Raghuvinder", "code": "7460", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Rajendran", "code": "9920", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Rajesh", "code": "1331", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Ramesh", "code": "7889", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Ray", "code": "9431", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Reeza", "code": "6440", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Rishad", "code": "7771", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Ritwik", "code": "7698", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Saarang", "code": "8651", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Sachin", "code": "1334", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Santosh", "code": "5237", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Shailendra", "code": "5396", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Shaina", "code": "8519", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Shambhavi", "code": "6726", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Shwetha", "code": "4043", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Swarna", "code": "2634", "fbReceived": 0, "fbSent": 0 },
-      { "name": "Vinayak", "code": "2043", "fbReceived": 0, "fbSent": 0 }
-    ];
-    addParticipantsToFirestore(pp);
-  }
+  // const caution = () => {
+  //   const pp = [
+  //     { "name": "Abhishek", "code": "9042", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Aditya", "code": "7836", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Amit", "code": "3388", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Aravind", "code": "1971", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Arnav", "code": "4880", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Ashwini", "code": "9322", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Atul", "code": "6643", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Chandrasekar", "code": "2723", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Chirag", "code": "6740", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "DhruvD", "code": "9668", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "DhruvK", "code": "5035", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Donnie", "code": "8702", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Emanda", "code": "9475", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Gowri", "code": "3624", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Hriday", "code": "2188", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Jitu", "code": "3992", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Kahraman", "code": "8446", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Kaimal", "code": "5444", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Karan", "code": "6256", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Kohli", "code": "8014", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Mandanna", "code": "9183", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Manjiri", "code": "1611", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Manoj", "code": "5596", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Maria", "code": "9210", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Naina", "code": "2386", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Naveen", "code": "6942", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Neel", "code": "2076", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Paul", "code": "3697", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Pradeep", "code": "9236", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Prasad", "code": "8425", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Priti", "code": "7219", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Raghavendra", "code": "6781", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Raghu", "code": "3924", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Raghuvinder", "code": "7460", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Rajendran", "code": "9920", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Rajesh", "code": "1331", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Ramesh", "code": "7889", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Ray", "code": "9431", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Reeza", "code": "6440", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Rishad", "code": "7771", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Ritwik", "code": "7698", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Saarang", "code": "8651", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Sachin", "code": "1334", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Santosh", "code": "5237", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Shailendra", "code": "5396", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Shaina", "code": "8519", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Shambhavi", "code": "6726", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Shwetha", "code": "4043", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Swarna", "code": "2634", "fbReceived": 0, "fbSent": 0 },
+  //     { "name": "Vinayak", "code": "2043", "fbReceived": 0, "fbSent": 0 }
+  //   ];
+  //   addParticipantsToFirestore(pp);
+  // }
   // Event handler for emailing a participant
   const handleEmailClick = (participantId) => {
     // console.log("Send email");
@@ -169,8 +169,9 @@ function SingleEventDashboard() {
           {/* <span className='flex'> */}
           <Button props={{
             "text": "+ Add Participant", bgColor: "bg-yellow-500",
-            "onClick": () => caution()
-            // navigate('/admin/add-participant')
+            "onClick": 
+            // () => caution()
+            navigate('/admin/add-participant')
           }} />
           <div className='m-2'></div>
           <Button props={{ "text": event.isEnded ? "Start Event" : "Stop Event", bgColor: event.isEnded ? "bg-green-500" : "bg-red-500", "onClick": () => handleStartEvent() }} />
